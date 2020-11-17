1e7dad6114dbb35e57a3ffe3363cef30fce3ef04
unknown <kjw970103@gmail.com>
2020-11-17 21:31:27 +0900

    resolve #<5>

--- hotel-db-system/UserApp/urls.py
+++ hotel-db-system/UserApp/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+from . import views
+
 
 app_name = "userapp"
 urlpatterns = [
-
+    path('login/', views.guest_login, name='guest_login'),
+    path('staff_login/', views.staff_login, name='staff_login'),
+    path('signup/', views.signup, name='guest_signup'),
+    path('signup_submit/', views.signup_submit, name='signup_submit'),
+    path('login_submit/', views.login_submit, name='login_submit'),
+    path('logout/', views.logout, name='logout'),
+    path('mypage/', views.guest_mypage, name='guest_mypage')
 ]
--- hotel-db-system/UserApp/views.py
+++ hotel-db-system/UserApp/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import auth
+from .models import Guest
+import json
+from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
+
+
+def guest_login(request):
+    return render(request, 'UserApp/login.html')
+
+
+def staff_login(request):
+    return render(request, 'UserApp/staff_login.html')
+
+
+def signup_submit(request):
+
+    if request.method == 'POST':
+
+        site_id = request.POST.get('id')
+        site_pw = request.POST.get('password')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+
+        guest = Guest()
+        guest.site_id = site_id
+        guest.site_pw = site_pw
+        guest.first_name = first_name
+        guest.last_name = last_name
+
+        guest.save()
+        return render(request, 'UserApp/login.html')
+    return render(request, 'UserApp/signup.html')
+
+
+def login_submit(request):
+    return render(request, 'UserApp/signup.html')
+
+
+def signup(request):
+    return render(request, 'UserApp/signup.html')
+
+
+def logout(request):
+    return render(request, 'UserApp/logout.html')
+
+
+def guest_mypage(request):
+    return render(request, 'UserApp/mypage.html')
