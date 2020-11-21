76205a2e21081bae4802d17fc7bd43b2a2a30ec4
lolmc <illionaire1004@gmail.com>
2020-11-21 23:21:20 +0900

    TaskApp URL 연결

--- hotel-db-system/hotel/urls.py
+++ hotel-db-system/hotel/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('userApp/', include('UserApp.urls')),
+    path('TaskApp/', include('TaskApp.urls')),
 ]
