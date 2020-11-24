a00491cf979183d82d96010e6c656caebffe3194
Hyeseon Lee <hyesun1999@gmail.com>
2020-11-24 16:55:03 +0900

    [backend] roomservice 메뉴 리스트 불러오기

--- hotel-db-system/padapp/views.py
+++ hotel-db-system/padapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import RoomServiceType, RoomService
 
 # Create your views here.
+# def pad_item_list(request):
+#     return redirect('padapp:pad')
+
+def room_service(request):
+    menu = RoomServiceType.objects.all()
+
+    return render(request, 'roomservice.html', {'menu':menu})
